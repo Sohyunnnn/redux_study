81dc8591d67f27e799baf08cd7064fe8efa78931
Sohyunnnn <thgus05061@naver.com>
2024-05-16 01:32:02 +0900

    #3.0 setup

--- react-redux/todo-list/src/routes/Home.js
+++ react-redux/todo-list/src/routes/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [text, setText] = useState('');
+
+  function onChage(e){
+    setText(e.target.value);
+  }
+
+  function onSubmit(e){
+    e.preventDefault();
+    console.log(text);
+    setText('');
+  }
+
   return (
-    <div>Home</div>
+    <>
+    <h1>To Do</h1>
+    <form onSubmit={onSubmit}>
+      <input type='text' value={text} onChange={onChage}/>
+      <button>Add</button>
+    </form>
+    <ul></ul>
+    </>
   )
 }
 
