393a8ef17a1913146e357dab6b2be1e63047089b
Sohyunnnn <thgus05061@naver.com>
2024-05-29 16:16:41 +0900

    #4.2 createReducer

--- redux-toolkit/todo-list/src/store.js
+++ redux-toolkit/todo-list/src/store.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 // 초기 상태를 스토리지에서 불러오기
 const initialState = JSON.parse(localStorage.getItem('todos')) || [];
@@ -10,17 +10,37 @@ const deleteToDo= createAction("DELETE");
 
 
 
-const reducer = (state= initialState, action) =>{
-  switch(action.type){
-    case addToDo.type: 
-      return [{text:action.payload, id: Date.now()}, ...state];
-    case deleteToDo.type:
-      return state.filter(toDo=> toDo.id !==action.payload);
-      default:
-        return state;
+// const reducer = (state= initialState, action) =>{
+//   switch(action.type){
+//     case addToDo.type: 
+//       return [{text:action.payload, id: Date.now()}, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo=> toDo.id !==action.payload);
+//       default:
+//         return state;
 
-  }
-}
+//   }
+// }
+
+// const reducer = createReducer([],{
+//   [addToDo] : (state, action)  => {
+//     state.push({text:action.payload, id: Date.now()}); //state mutate
+//   },
+//   [deleteToDo] : (state, action) => 
+//     state.filter(toDo=> toDo.id !==action.payload)
+  
+// })
+
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) => {
+      return state.filter(toDo => toDo.id !== action.payload);
+    });
+});
+
 
 
 const store= createStore(reducer);
